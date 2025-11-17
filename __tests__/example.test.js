a5d214279f39f4398b6629ea008ef814fe37d8a0
Sxkshm223 <156046546+Sxkshm223@users.noreply.github.com>
2025-11-17 09:47:36 +0530

    Update example.test.js

--- __tests__/example.test.js
+++ __tests__/example.test.js
@@ -7,5 +7,7 @@ describe('Example Unit Test', () => {
   it('should add two numbers correctly', () => {
     // Assert that the result of add(1, 2) is 3
     expect(add(1, 2)).toBe(3);
+    //test
   });
+
 });
