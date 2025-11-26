7c2935744ee2dab6fb238f18849b766c54408dc4
sehaj774 <sehju344@gmail.com>
2025-11-26 10:44:09 +0530

    Update pgdb.js

--- config/pgdb.js
+++ config/pgdb.js
@@ -1,25 +1,23 @@
 const { Pool } = require('pg');
 
-// // --- TEMPORARY DEBUG CODE ---
-// console.log('--- Checking PostgreSQL ENV Variables ---');
-// console.log('User:', process.env.PG_USER);
-// console.log('Host:', process.env.PG_HOST);
-// console.log('Database:', process.env.PG_DATABASE);
-// console.log('Password:', process.env.PG_PASSWORD);
-// console.log('Port:', process.env.PG_PORT);
-// console.log('------------------------------------');
-// // --- END DEBUG CODE ---
-
 const pool = new Pool({
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT,
+  // FIX: Neon DB requires SSL.
+  // If the host is NOT localhost, we assume it's a cloud DB and enable SSL.
+  ssl: process.env.PG_HOST !== 'localhost' ? { rejectUnauthorized: false } : false
 });
 
 pool.on('connect', () => {
   console.log('PostgreSQL Connected...');
+});
+
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
 });
 
 module.exports = {
